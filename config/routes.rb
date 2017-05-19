b327b89d90d875d55c0c302892847ceedee78c8f
Marco Castro <marcodotcastro@gmail.com>
2017-05-19 17:35:18 -0300

    crud de cliente criado com layout simples

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root "clientes#index"
+
+  resources :clientes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
