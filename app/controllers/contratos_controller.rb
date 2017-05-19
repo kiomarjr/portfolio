28e585897f2d1e54630adce9522e091a822bf93e
Marco Castro <marcodotcastro@gmail.com>
2017-05-19 18:13:07 -0300

    adicionado cliente no form do contrato

--- app/controllers/contratos_controller.rb
+++ app/controllers/contratos_controller.rb
@@ -69,6 +69,6 @@ class ContratosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def contrato_params
-      params.require(:contrato).permit(:numero)
+      params.require(:contrato).permit(:numero, :cliente_id)
     end
 end
